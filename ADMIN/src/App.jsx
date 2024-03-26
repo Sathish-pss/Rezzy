7931dfed237bf8e9f0f791216313a273f7a8c0b2
SATHISH G -PSS <133330215+Sathish-pss@users.noreply.github.com>
2024-03-26 19:06:17 +0530

    Created React APP
    => Created a Seperate Applicaton for the Admin app
    => Created a seperate Application for the User App

---
+++ ADMIN/src/App.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>Welcome to Admin App for the Reservation Application</Fragment>
+  );
+}
+
+export default App;
---
+++ USER/src/App.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>Welcome to USER Applicaton for the Reservation App</Fragment>
+  );
+}
+
+export default App;
